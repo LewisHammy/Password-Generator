902af0e0a58f34f20421e2edca167d141f3043ee
LewisHammy <samkabir30@gmail.com>
2023-07-18 17:50:07 -0700

    solved bug by adding logic for generating a password, also included features like prompt questions

--- 03-Challenge/Develop/script.js
+++ 03-Challenge/Develop/script.js
@@ -1,14 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+function generatePassword() {
+  var length = prompt("Enter the length of the password (between 8 and 128 characters):");
+  length = parseInt(length);
+
+  // Validate the length input
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid length! Please enter a number between 8 and 128.");
+    return "";
+  }
+
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  // Validate that at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("Please select at least one character type.");
+    return "";
+  }
+
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+";
+
+  var availableChars = "";
+  if (includeLowercase) {
+    availableChars += lowercaseChars;
+  }
+  if (includeUppercase) {
+    availableChars += uppercaseChars;
+  }
+  if (includeNumeric) {
+    availableChars += numericChars;
+  }
+  if (includeSpecial) {
+    availableChars += specialChars;
+  }
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * availableChars.length);
+    password += availableChars.charAt(randomIndex);
+  }
+
+  return password;
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
